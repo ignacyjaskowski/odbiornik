251b79706e3e3d53d38bdae319053760c685f071
ignacyjaskowski <74858286+ignacyjaskowski@users.noreply.github.com>
2020-11-22 12:09:10 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,8 +4,16 @@ radio.onReceivedNumber(function (receivedNumber) {
         powerfunctions.setSpeed(PowerFunctionsMotor.Red1, n)
     } else if (receivedNumber == 1) {
         a = a - 1
+        powerfunctions.setSpeed(PowerFunctionsMotor.Blue1, a)
     } else if (receivedNumber == 2) {
-    	
+        powerfunctions.setSpeed(PowerFunctionsMotor.Red1, 0)
+        powerfunctions.setSpeed(PowerFunctionsMotor.Blue1, 0)
+    } else if (receivedNumber == 3) {
+        n = n - 1
+        powerfunctions.setSpeed(PowerFunctionsMotor.Red1, n)
+    } else if (receivedNumber == 4) {
+        a = a + 1
+        powerfunctions.setSpeed(PowerFunctionsMotor.Blue1, a)
     }
 })
 let a = 0
